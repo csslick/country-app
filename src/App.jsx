ccba2dc4c7d4e4a73f54ead163e923802a98c90a
csslick <digitone@naver.com>
2023-04-10 22:00:08 +0900

    search

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,30 @@ import Header from './components/Header';
 function App() {
   const [dark, setDark] = useState(false);
   const [data, setData] = useState([]);
+  const [searchOption, setSearchOption] = useState('all');
   const handleDarkMode = () => {
     setDark(!dark);
+  }
+
+  const handleSearchOption = (name) => {
+    setSearchOption(name);
+    console.log(name)
   }
   
   useEffect(() => {
     // 국가코드별: https://restcountries.com/v3.1/alpha/fr
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => {
-        setData(data)
+    fetch(`https://restcountries.com/v3.1/${searchOption}`)
+      .then(res => {
+        if(res.ok) {
+          return res.json()
+        } else {
+          alert('찾는 데이터가 없습니다!');
+          throw new Error(`${res.status} 에러`);
+        }
       })
-  }, [])
+      .then(data => {setData(data)})
+      .catch(err => console.log(err.message))
+  }, [searchOption])
   console.log(data);
 
   return (
@@ -27,7 +39,11 @@ function App() {
       <Header handleDarkMode={handleDarkMode} />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home data={data} />} />
+          <Route 
+            path='/' 
+            element={<Home data={data} 
+            handleSearchOption={handleSearchOption} 
+          />} />
           <Route path='/detail/:id' element={<Detail />} />
         </Routes>
       </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Home(props) {
-  const { data } = props;
+  const { data, handleSearchOption } = props;
+  const search = useRef();
+  const [ input, setInput ] = useState('');
+  // console.log(search.current);
 
   return (
     <div>
-      <form action="/detail/fr" className='container'>
-        <input type="search" placeholder='search' size="30" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSearchOption(`name/${input}`);
+          setInput('');
+        }} 
+        className='search-ui container'>
+        <input 
+          type="search" 
+          placeholder='search' 
+          size="30"
+          ref={search}
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
         <button className="btn" type="submit">search</button>
       </form>
       <section className="main flex-container">
@@ -27,7 +45,7 @@ export default function Home(props) {
                   <p className="population"><b>Population: </b>{country.population.toLocaleString()}</p>
                   <p className='region'><b>Region: </b>{country.continents[0]}</p>
                   <p className="capital">
-                    <b>Capital: </b>{country.capital ? country.capital[0] : "" }
+                    <b>Capital: </b>{country.capital ? country.capital[0] : null }
                   </p>
                 </div>
               </Link>
